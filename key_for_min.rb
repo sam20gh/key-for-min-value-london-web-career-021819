96e4118392e92d0b81148db4fd5935e3ac945937
sam ghoreishi <sam20gh@gmail.com>
2019-02-13 01:03:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,18 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  return nil if name_hash == {}
-  temp_key = ""
-  temp_value = ""
-  name_hash.each do |key, value| #blake: 500
-    if (temp_value == "") #if first time we run and temp_value hasnt been assigned, assign it
-      temp_key = key
-      temp_value = value
-
-    elsif (value < temp_value) #if we have a temp_value, compare the current value to it
-      temp_key = key
-      temp_value = value
+  names = nil
+  name_hash.collect do |name, num|
+    smallest = num[0]
+    if num < smallest
+      names = name
     end
   end
-  temp_key
+  names
 end
